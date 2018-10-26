9326d21a9d3bb2a604168659496bb4119b53b051
Paul <paul@armory.io>
2018-10-26 11:31:59 -0700

    Bump the OSS comparison to 1.9

--- policy-diff.sh
+++ policy-diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /spinnaker
 cd /spinnaker
-git clone -b release-1.8.x https://github.com/spinnaker/clouddriver.git 1>&2
+git clone -b release-1.9.x https://github.com/spinnaker/clouddriver.git 1>&2
 echo "executing policy-diff"
 /src/policy_diff.py /spinnaker/clouddriver/clouddriver-aws
